6a6209e79630b629a134466187fd71c5a7698c3a
Shivam Agrawal <shivam96.sa@gmail.com>
2020-11-13 00:04:44 +0530

    Implemented Components and Databinding - Parent to Child data passing

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
     selector: 'app-header',
     templateUrl: 'header.component.html'
 })
 export class HeaderComponent implements OnInit {
+    @Output() featureSelected = new EventEmitter<string>();
 
     constructor() { }
+
+    navigateTo(feature: string) {
+        this.featureSelected.emit(feature);
+    }
 
     ngOnInit() { 
 
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,13 +7,17 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe("A new Recipe", "Test Description", "https://media.self.com/photos/5f1eef2914b005b8d8eba4d0/4:3/w_384/30-Minute-Roasted-Vegetable-Tacos-with-Chimichurri-BIG-flavor-satisfying-HEALTHY-vegan-glutenfree-plantbased-tacos-chimichurri-cauliflower-minimalistbaker-recipe-6.jpg"),
-    new Recipe("A new Recipe", "Test Description", "https://media.self.com/photos/5f1eef2914b005b8d8eba4d0/4:3/w_384/30-Minute-Roasted-Vegetable-Tacos-with-Chimichurri-BIG-flavor-satisfying-HEALTHY-vegan-glutenfree-plantbased-tacos-chimichurri-cauliflower-minimalistbaker-recipe-6.jpg")
+    new Recipe("Another new Recipe", "Another Test Description", "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=960,872")
 
   ];
   constructor() { }
+
+  onRecipeWasSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 
   ngOnInit(): void {
   }
